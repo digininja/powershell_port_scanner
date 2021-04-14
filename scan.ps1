179a7e2bb3380a364393d8d974fb0d240d6b1324
Robin Wood <robin@digi.ninja>
2021-04-14 13:50:46 +0100

    Improve usage instructions

--- scan.ps1
+++ scan.ps1
@@ -1,7 +1,5 @@
-# To use the scanner module, import-module scanner.ps1
-# That can be used on its own:
-# Test-Port digi.ninja 443 1000
-# Check digi.ninja port 443 and with a 1000ms (1second) timeout
+# Before using this, import the scanner module:
+# Import-Module scanner.ps1
 
 # To capture the output, can use Tee-Object
 # .\scan.ps1 | Tee-Object grabbed
@@ -14,7 +12,7 @@ $ports=(21,22,23,80,443,445,3389)
     echo "------";
     echo "$ip";
     foreach ($port in $ports) {
-        # This uses the built in stuff
+        # This uses the built in PowerShell stuff
         # echo ((new-object Net.Sockets.TcpClient).Connect($ip,$port)) "Port $port is open!"
 
         # This uses the scanner module
